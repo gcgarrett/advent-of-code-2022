152c91cb23e7ba188097729c17b30dbb2432534a
Gregory Garrett <gcgarrett@gregorys-mbp.attlocal.net>
2023-02-09 13:35:54 -0500

    Updates code to solve part 2 of the day 1 puzzle. Instead of returning the top calorie count, returns a sum of the top three counts

--- day1/calorie_max.py
+++ day1/calorie_max.py
@@ -1,30 +1,36 @@
 f = open('./puzzle_input.txt')
 
 currentCalories = 0
-caloriesMax = 0
+caloriesList = []
 
+# loop through the input file
 while(True):
+    # get next line
     line = f.readline()
 
+    # append the current calories count to the list if we are looking at a new
+    # elf or are at the end of the file
     if line == "" or line == "\n":
-        if currentCalories > caloriesMax:
-            caloriesMax = currentCalories
+        caloriesList.append(currentCalories)
         currentCalories = 0
 
+    # EOF, break out of the loop
     if line == "":
-        print("--EOF--")
         break
 
+    # New elf, continue to the next one
     if line == "\n":
-        print("--NEW ELF--")
         continue
-    
-    calories = int(line.strip())
-    print("--CALORIES--")
-    print(calories)
-    currentCalories += calories
-    print("--CURRENT--")
-    print(currentCalories)
 
-print("--MAX CALORIES--")
-print(caloriesMax)
+    # convert the line from a string to an integer
+    calories = int(line.strip())
+    # add value to running total
+    currentCalories += calories
+
+# sort list in descending order
+caloriesList.sort(reverse=True)
+# sum just the top three calorie counts
+topCaloriesTotal = sum(caloriesList[:3])
+
+print("--TOP CALORIES TOTAL--")
+print(topCaloriesTotal)
